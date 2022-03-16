6df2aa32dea8e3c9981f31d626aa1f87a2ff340f
anshul4khare <anshul.khare040284@gmail.com>
2022-03-16 19:34:08 +0530

    assignment submission

---
+++ home.js
@@ -0,0 +1,8 @@
+function loadCoupon() {document.getElementById('Coupon').style.visibility= 'visible'}
+
+function CloseCoupon() {document.getElementById('Coupon').style.visibility= 'hidden'}
+
+function changeMode() {
+  var mybody = document.body;
+  mybody.classList.toggle('mydark')
+}
